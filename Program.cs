be4cc150e2969590af51a48337755428481e0f97
FabianoEtec <nbscbs@gmail.com>
2021-02-19 22:11:38 -0300

    Primeira aula C# Etec

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ola_Mundo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Seja bem vindo Fabiano!");
+
+        }
+    }
+}
